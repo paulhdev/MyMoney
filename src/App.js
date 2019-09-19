c38eb6b4c46c1ec44102c80924d7eaf41140276a
PauloRev <paulohenriquerev@gmail.com>
2019-09-19 20:59:16 -0300

    project start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Rest from "./rest";
+
+const baseURL = "https://mymoney-phdev.firebaseio.com/";
+
+const { useGet } = Rest(baseURL);
 
 function App() {
+  const data = useGet("meses");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container">
+          <h5 className="navbar-brand">MyMoney</h5>
+        </div>
+      </nav>
+      <h2>Adicionar mês</h2>
+      <select>
+        <option value="2019">2019</option>
+        <option value="2020">2020</option>
+      </select>
+      <select>
+        <option value="01">01</option>
+        <option value="02">02</option>
+      </select>
+      <button>Adicionar Mês</button>
+
+      <pre>{JSON.stringify(data)}</pre>
     </div>
   );
 }
